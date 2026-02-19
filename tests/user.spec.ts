883d8cf5cd5953b92a9a70cb76bd083556f72c3e
Zack <zack.r.clifford@gmail.com>
2026-02-19 13:32:13 -0700

    cleanup old comments

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,50 +1,5 @@
-// only works with backend running rn -> when pushing either have it running
-// or change this up to mock
-
 import { test, expect } from 'playwright-test-coverage';
 import { basicInit } from './basicInit'
-
-// test('updateUser', async ({ page }) => { 
-//     const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
-//     await page.goto('/');
-//     await page.getByRole('link', { name: 'Register' }).click();
-//     await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
-//     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
-//     await page.getByRole('textbox', { name: 'Password' }).fill('diner');
-//     await page.getByRole('button', { name: 'Register' }).click();
-
-//     await page.getByRole('link', { name: 'pd' }).click();
-
-//     await expect(page.getByRole('main')).toContainText('pizza diner');
-
-//     await page.getByRole('button', { name: 'Edit' }).click();
-//     await expect(page.locator('h3')).toContainText('Edit user');
-//     await page.getByRole('button', { name: 'Update' }).click();
-
-//     await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
-
-//     await expect(page.getByRole('main')).toContainText('pizza diner');
-
-//     await page.getByRole('button', { name: 'Edit' }).click();
-//     await expect(page.locator('h3')).toContainText('Edit user');
-//     await page.getByRole('textbox').first().fill('pizza dinerx');
-//     await page.getByRole('button', { name: 'Update' }).click();
-
-//     await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
-
-//     await expect(page.getByRole('main')).toContainText('pizza dinerx');
-
-//     await page.getByRole('link', { name: 'Logout' }).click();
-//     await page.getByRole('link', { name: 'Login' }).click();
-
-//     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
-//     await page.getByRole('textbox', { name: 'Password' }).fill('diner');
-//     await page.getByRole('button', { name: 'Login' }).click();
-
-//     await page.getByRole('link', { name: 'pd' }).click();
-
-//     await expect(page.getByRole('main')).toContainText('pizza dinerx');
-// });
 
 test('update user name', async ({ page }) => {
     await basicInit(page);
@@ -148,11 +103,3 @@ test('diner cannot change roles', async ({ page }) => {
         page.getByRole('combobox', { name: /role/i })
     ).toHaveCount(0);
 });
-
-/*
-There are still other tests that we need to write
-in order for us to be fully comfortable with the new update user functionality. 
-This includes changing the password and email address, 
-and changing user information using different roles. 
-Go ahead and write those tests now and commit those changes also.
-*/
